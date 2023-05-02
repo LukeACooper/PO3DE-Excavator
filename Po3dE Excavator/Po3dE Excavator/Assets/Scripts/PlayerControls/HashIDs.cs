0cd65ae3f4a40c582307a5ccb4cdb2cc31bb9d7e
Luke <luke6.cooper@live.uwe.ac.uk>
2023-05-02 13:38:00 +0100

    Player Movement & Anims
    Plus terrain changes/additions (+cave items)

---
+++ Po3dE Excavator/Po3dE Excavator/Assets/Scripts/PlayerControls/HashIDs.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashIDs : MonoBehaviour
+{
+    public int dyingState;
+    public int deadBool;
+    public int walkState;
+    public int shoutState;
+    public int speedFloat;
+    public int sneakingBool;
+    public int shoutingBool;
+
+    private void Awake()
+    {
+        dyingState = Animator.StringToHash("BaseLayer.Dying");
+        deadBool = Animator.StringToHash("Dead");
+        walkState = Animator.StringToHash("Walk");
+        shoutState = Animator.StringToHash("Shout");
+        speedFloat = Animator.StringToHash("Speed");
+        sneakingBool = Animator.StringToHash("Sneaking");
+        shoutingBool = Animator.StringToHash("Shouting");
+    }
+}
